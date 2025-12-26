ef75c785c09cf891fccf9642e10abe5514af18a8
Erik Takoev <eriktakoev@gmail.com>
2025-12-26 18:21:01 +0200

    Add constructor for Optional

--- Optional/Optional.cs
+++ Optional/Optional.cs
@@ -3,31 +3,39 @@ using UnityEngine;
 
 namespace Utils
 {
-    // <summary>
-    // Optional is a generic class that can be used to represent an optional value.
-    // It contains a boolean flag to indicate whether the value is enabled or not.
-    [System.Serializable]
-    public class Optional<T> : ISerializationCallbackReceiver where T : new()
-    {
+	// <summary>
+	// Optional is a generic class that can be used to represent an optional value.
+	// It contains a boolean flag to indicate whether the value is enabled or not.
+	[System.Serializable]
+	public class Optional<T> : ISerializationCallbackReceiver where T : new()
+	{
 
-        public bool Enabled = false;
-        public T Value;
+		public bool Enabled = false;
+		public T Value;
 
-        public Optional()
-        {
-            Value = new();
-        }
+		public Optional()
+		{
+			Value = new();
+		}
+		public Optional(T value)
+		{
+			Value = value;
+			if (Value != null)
+			{
+				Enabled = true;
+			}
+		}
 
-        public void OnAfterDeserialize()
-        {
-        }
+		public void OnAfterDeserialize()
+		{
+		}
 
-        public void OnBeforeSerialize()
-        {
-            if (!Enabled)
-            {
-                Value = default;
-            }
-        }
-    }
+		public void OnBeforeSerialize()
+		{
+			if (!Enabled)
+			{
+				Value = default;
+			}
+		}
+	}
 }
